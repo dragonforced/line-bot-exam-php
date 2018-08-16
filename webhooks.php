ded63c879eded81405d428826ca6979a44157ff1
dragonforced <balasamuri19@gmail.com>
2018-08-16 13:53:17 +0700

    Change token

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'k0GW1kDqdCnUStcbSEPUFDDwMcD2CsqnlK5UD4BpdYsn3edbY7mCx/yLMP2a5NMuNKwa8kb5IdQk7qZAF6+6MST9aUvbF907iy3QUFOToCrb3J2nVnfdYsLjaG04kqD6fR4pGT6I0qlBjcmgBXPzJAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
