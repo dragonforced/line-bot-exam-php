9d6e6d0d647786c176b258c0da62a7c08933cf3d
dragonforced <balasamuri19@gmail.com>
2018-08-16 15:00:32 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -26,27 +26,28 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => $text
 			];
-			/*$messages = [
-				"type": "flex",
-				"altText": "This is a Flex Message",
-				"contents": {
-					"type": "bubble",
-					"body": {
-						"type": "box",
-						"layout": "vertical",
-						"contents": [
-							{
-								"type": "text",
-								"text": "Hello,"
-							},
-							{
-								"type": "text",
-								"text": "World!"
-							}
+			
+			$messages = [
+				"type" => "flex",
+				"altText" => "This is a Flex Message",
+				"contents" => [
+					"type" => "bubble",
+					"body" => [
+						"type" => "box",
+						"layout" => "vertical",
+						"contents" => [
+							[
+								"type" => "text",
+								"text" => "Hello,"
+							],
+							[
+								"type" => "text",
+								"text" => "World!"
+							]
 						]
-					}
-				}
-			];*/
+					]
+				]
+			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
